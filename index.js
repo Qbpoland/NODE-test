fbc391f50153ec7d08e753bf2c9082b91907ea7b
Qbpoland <jakub.stasiuk2005@gmail.com>
2023-09-11 13:09:46 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+
+console.log('Startuje serwer...');
+
+http.createServer(function (request, response){
+    //tu jest funkcja createServer
+    //request - żądanie wysłane z przeglądarki 
+    //response - odpowiedź wysyłana do przeglądarki
+    response.writeHead(418, {'Content-Type': 'text/html'});
+    response.write("Hello World");
+    response.end();
+
+}).listen(8080);
